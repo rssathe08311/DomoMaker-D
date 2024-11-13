02ab13a051a055fd50ec8b8360b0f32d602fa1de
Reva Sathe <rss9622@g.rit.edu>
2024-11-13 18:29:01 -0500

    started DomoMaker E

---
+++ server/models/Drink.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const _ = require('underscore');
+
+const setName = (name) => _.escape(name).trim();
+
+const DrinkSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    set: setName,
+  },
+  temperature: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  owner: {
+    type: mongoose.Schema.ObjectId,
+    required: true,
+    ref: 'Account',
+  },
+  createdDate: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+DrinkSchema.statics.toAPI = (doc) => ({
+  name: doc.name,
+  temperature: doc.temperature,
+});
+
+const DrinkModel = mongoose.model('Drink', DrinkSchema);
+module.exports = DrinkModel;
